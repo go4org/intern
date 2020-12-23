969c7e87e7cb0c49d40cf3cdc96ce271534c96ea
David Anderson <dave@natulte.net>
2020-12-22 21:53:18 -0800

    Update documentation to reflect the new implementation.
    Signed-off-by: David Anderson <dave@natulte.net>

--- intern.go
+++ intern.go
@@ -99,48 +99,46 @@ func finalize(v *Value) {
 	delete(valMap, v.cmpVal)
 }
 
-// Interning is simple if you don't require that unused values be garbage collectable.
-// But we do require that; we don't want to be DOS vector.
-// We do this by using a uintptr to hide the pointer from the garbage collector,
-// and using a finalizer to eliminate the pointer when no other code is using it.
+// Interning is simple if you don't require that unused values be
+// garbage collectable. But we do require that; we don't want to be
+// DOS vector. We do this by using a uintptr to hide the pointer from
+// the garbage collector, and using a finalizer to eliminate the
+// pointer when no other code is using it.
 //
-// The obvious implementation of this is to use a map[interface{}]uintptr-of-*interface{},
-// and set up a finalizer to delete from the map.
-// Unfortunately, that contains a logical race.
-// The finalizer can start concurrently with a new request
-// to look up a pointer with no other references to it.
-// The new pointer lookup creates a new reference to an existing (almost-GC-able) pointer.
-// The finalizer then continues to run, deleting the pointer from the map.
-// Future pointer lookups will create a new pointer, breaking the comparability invariant.
+// The obvious implementation of this is to use a
+// map[interface{}]uintptr-of-*interface{}, and set up a finalizer to
+// delete from the map. Unfortunately, this is racy. Because pointers
+// are being created in violation of Go's unsafety rules, it's
+// possible to create a pointer to a value concurrently with the GC
+// concluding that the value can be collected. There are other races
+// that break the equality invariant as well, but the use-after-free
+// will cause a runtime crash.
 //
-// The finalizer fundamentally needs to know that no other
-// references have been created since this finalizer was set up.
-// There is no external synchronization that can provide that.
-// Instead, every time we create a new Value pointer, we set a new finalizer.
-// That finalizer knows the latest Value pointer at the time that it was created;
-// that is the gen (generation) field in type Value.
-// When the finalizer runs, if its generation differs from the current Value generation,
-// another reference must have been created in the interim,
-// so it should not delete the *Value from the map.
-// Another, later, finalizer will take care of that.
-// The Value generation field is protected by mu, providing a consistent view.
+// To make this work, the finalizer needs to know that no references
+// have been unsafely created since the finalizer was set up. To do
+// this, values carry a "resurrected" sentinel, which gets set
+// whenever a pointer is unsafely created. If the finalizer encounters
+// the sentinel, it clears the sentinel and delays collection for one
+// additional GC cycle, by re-installing itself as finalizer. This
+// ensures that the unsafely created pointer is visible to the GC, and
+// will correctly prevent collection.
 //
-// @josharian has a mild lingering concern about this approach.
-// It is possible to for the runtime to concurrently decide it needs to _execute_ a finalizer and
-// also _remove_ the need for that finalizer to run, because a new reference has appeared.
-// It is possible that this could cause a data race in the runtime.
-// This is not a normal thing to have happen; it requires unsafe hiding of a pointer in a uintptr.
-// It thus might not be tested for or protected against in the runtime.
-// Hopefully this will not prove to be a problem in practice.
+// This technique does mean that interned values that get reused take
+// at least 3 GC cycles to fully collect (1 to clear the sentinel, 1
+// to clean up the unsafe map, 1 to be actually deleted).
 //
-// @ianlancetaylor commented in https://github.com/golang/go/issues/41303#issuecomment-717401656
-// that it is possible to implement weak references in terms of finalizers without unsafe.
-// Unfortunately, the approach he outlined does not work here, for two reasons.
-// First, there is no way to construct a strong pointer out of a weak pointer;
-// our map stores weak pointers, but we must return strong pointers to callers.
-// Second, and more fundamentally, we must return not just _a_ strong pointer to callers,
-// but _the same_ strong pointer to callers.
-// In order to return _the same_ strong pointer to callers, we must track it,
-// which is exactly what we cannot do with strong pointers.
+// @ianlancetaylor commented in
+// https://github.com/golang/go/issues/41303#issuecomment-717401656
+// that it is possible to implement weak references in terms of
+// finalizers without unsafe. Unfortunately, the approach he outlined
+// does not work here, for two reasons. First, there is no way to
+// construct a strong pointer out of a weak pointer; our map stores
+// weak pointers, but we must return strong pointers to callers.
+// Second, and more fundamentally, we must return not just _a_ strong
+// pointer to callers, but _the same_ strong pointer to callers. In
+// order to return _the same_ strong pointer to callers, we must track
+// it, which is exactly what we cannot do with strong pointers.
 //
-// See https://github.com/inetaf/netaddr/issues/53 for more discussion.
+// See https://github.com/inetaf/netaddr/issues/53 for more
+// discussion, and https://github.com/go4org/intern/issues/2 for an
+// illustration of the subtleties at play.
